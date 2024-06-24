5a1b0b3ec6967d54946a65e7e7f4e7ca26e32ec0
YohanesKefi <72695877+yolla231201@users.noreply.github.com>
2024-06-24 23:35:04 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <section>
     <MainCover id="mainCover"></MainCover>
-    <!-- <FirstSight></FirstSight> -->
     <GroomBride></GroomBride>
     <!-- <Timelines v-on:goToGuestBook="navigationHandler('#guest-book')"></Timelines> -->
     <Timelines></Timelines>
     <OurGallery></OurGallery>
     <!-- <Envelope id="envelope"></Envelope> -->
     <!-- <GuestBook id="guest-book"></GuestBook> -->
+    <!-- <FirstSight></FirstSight> -->
+    <Last></Last>
   </section>
 </template>
 
@@ -17,6 +18,7 @@ import MainCover from '@/components/MainCover.vue'
 import FirstSight from '@/components/FirstSight.vue'
 import GroomBride from '@/components/GroomBride.vue'
 import Timelines from '@/components/Timelines.vue'
+import Last from '@/components/Last.vue'
 import OurGallery from '@/components/OurGallery.vue'
 import Envelope from '@/components/Envelope.vue'
 import GuestBook from '@/components/GuestBook.vue'
